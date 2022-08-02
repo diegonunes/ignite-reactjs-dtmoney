5616aed9e707797cf2741fe5e57b6f0d30ba47ab
Diego Nunes <diegonunesdev@gmail.com>
2022-08-02 01:02:34 -0300

    finished chapter II

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { Summary } from '../Summary';
+import { TransactionsTable } from '../TransactionsTable';
+import { Container } from './styles';
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  );
+};
+export default Dashboard;
